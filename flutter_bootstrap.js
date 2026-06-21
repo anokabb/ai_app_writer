e7594d2a7771350280686e1c0a94328200ab48ae
anokabb <anokabb@users.noreply.github.com>
2026-06-21 16:13:57 +0000

    deploy: cd22d17da5b2a5181dacd6e0d0dd68262c0b35b9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1488659838"
+    serviceWorkerVersion: "3896530537"
   }
 });
